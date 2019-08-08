fc6b98863a8592dacadbce5cca2e4d5005fb0d62
Sam Szreter <s.szreter@gmail.com>
2019-08-08 21:43:14 +0300

    Add button roles where appropriate, extract url constants

--- site/index.js
+++ site/index.js
@@ -5,8 +5,34 @@ const { Kanjiapi } = require('kanjiapi-wrapper')
 const kanjiapi = Kanjiapi.build()
 kanjiapi.addListener('app', m.redraw)
 
+const KYOIKU_URL = 'https://en.wikipedia.org/wiki/Ky%C5%8Diku_kanji'
+const JOYO_URL = 'https://en.wikipedia.org/wiki/J%C5%8Dy%C5%8D_kanji'
+const JINMEIIYO_URL = 'https://en.wikipedia.org/wiki/Jinmeiy%C5%8D_kanji'
+const GITHUB_URL = 'https://github.com/onlyskin/kanjiapi.dev'
+const KANJI_URL = 'https://en.wikipedia.org/wiki/Kanji'
+const MITHRIL_URL = 'https://mithril.js.org/'
+const JLPT_URL = 'https://en.wikipedia.org/wiki/Japanese-Language_Proficiency_Test#Previous_format_(1984%E2%80%932009)'
+const UNICODE_URL = 'https://en.wikipedia.org/wiki/Unicode'
+const KANJIAPI_WRAPPER_URL = 'https://github.com/onlyskin/kanjiapi-wrapper'
+const KANJIKAI_URL = 'https://kai.kanjiapi.dev'
+const EDRDG_URL = 'http://www.edrdg.org/'
+const EDRDG_LICENCE_URL = 'http://www.edrdg.org/edrdg/licence.html'
+const KANJIAPI_V1_URL = 'https://kanjiapi.dev/v1/'
+
 const Link = {
-    view: ({ children, attrs: { href } }) => m('a.vermillion', { href }, children),
+    view: ({ children, attrs: { href } }) => m(
+        'a.vermillion',
+        { role: 'button', href },
+        children
+    ),
+}
+
+const InternalLink = {
+    view: ({ children, attrs: { href } }) => m(
+        m.route.Link,
+        { role: 'button', href, class: 'vermillion' },
+        children
+    ),
 }
 
 const GithubIcon = {
@@ -24,22 +50,23 @@ const GithubIcon = {
             m('path', {
                 'd': 'M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12'
             }),
-        ]
-    )
+        ],
+    ),
 }
 
 const Footer = {
     view: () => m(
         '',
         { style: { 'height': '50px' } },
-        m(Link, { href: 'https://github.com/onlyskin/kanjiapi.dev' }, m(GithubIcon)),
-    )
+        m(Link, { href: GITHUB_URL }, m(GithubIcon)),
+    ),
 }
 
 const Header = {
     view: () => m(
         '.border-box.pa2.pa3-ns.white.w-100.bg-dark-ink',
-        [ m('.tc.ma1.f2.f1-ns.rounded', '漢字'), m('.tc.ma1.f2.f1-ns', 'kanjiapi.dev') ],
+        m('.tc.ma1.f2.f1-ns.rounded', '漢字'),
+        m('.tc.ma1.f2.f1-ns', 'kanjiapi.dev'),
     )
 }
 
@@ -49,76 +76,79 @@ const About = {
         m(
             '.self-start.pv2',
             'This is an API that provides JSON endpoints for over 13,000 ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/Kanji' }, 'kanji'),
+            m(Link, { href: KANJI_URL }, 'kanji'),
             ' using data from an extensive kanji dictionary.'
         ),
         m(
             '.self-start.pv2',
             'This API uses the EDICT and KANJIDIC dictionary files. ',
             'These files are the property of the ',
-            m(Link, { href: 'http://www.edrdg.org/' },
-                'Electronic Dictionary Research and Development Group'),
+            m(
+                Link,
+                { href: EDRDG_URL },
+                'Electronic Dictionary Research and Development Group',
+            ),
             ', and are used in conformance with the Group\'s ',
-            m(Link, { href: 'http://www.edrdg.org/edrdg/licence.html' },
-                'licence'),
+            m(Link, { href: EDRDG_LICENCE_URL }, 'licence'),
             '.',
         ),
         m(
             '.self-start.pv2',
             'This page is built using ',
-            m(Link, { href: 'https://mithril.js.org/' }, 'mithril.js'),
+            m(Link, { href: MITHRIL_URL }, 'mithril.js'),
             '.',
         ),
-    ]
+    ],
 }
 
 const Separator = {
-    view: () => m('hr.w-100.black-50.mv3')
+    view: () => m('hr.w-100.black-50.mv3'),
 }
 
 const Search = {
     view: ({ attrs: { path } }) => m(
         '.w-100.flex.items-stretch.mt1.mb0',
-        [
-            m('label#search-label', 'https://kanjiapi.dev/v1/'),
-            m(
-                'input[type=text]#search-input',
-                {
-                    value: path(),
-                    onchange: e => path(e.target.value),
-                },
-            ),
-        ],
+        m('label#search-label', KANJIAPI_V1_URL),
+        m(
+            'input[type=text]#search-input',
+            {
+                value: path(),
+                onchange: e => path(e.target.value),
+            },
+        ),
     ),
 }
 
 const Example = {
     view: ({ attrs: { path, url } }) => m(
         '.di.i.underline.nowrap.vermillion',
-        { onclick: () => path(url) },
+        { role: 'button',  onclick: () => path(url) },
         url,
-    )
+    ),
 }
 
 const Examples = {
     view: ({ attrs: { path } }) => m(
         '.f7.f6-ns.self-start.mt0.mb1',
-        [
-            'Hint: try ',
-            m(Example, { path, url: 'kanji/蜜' }),
-            ', ',
-            m(Example, { path, url: 'kanji/grade-1' }),
-            ', ',
-            m(Example, { path, url: 'reading/あり' }),
-            ', or ',
-            m(Example, { path, url: 'words/蠍' }),
-        ],
+        'Hint: try ',
+        m(Example, { path, url: 'kanji/蜜' }),
+        ', ',
+        m(Example, { path, url: 'kanji/grade-1' }),
+        ', ',
+        m(Example, { path, url: 'reading/あり' }),
+        ', or ',
+        m(Example, { path, url: 'words/蠍' }),
     ),
 }
 
 const SearchResult = {
     view: ({ attrs: { result } }) => m(
-        '.self-stretch.f7.f6-ns.pa1.pa2-ns.mv1.ba.b--black-10.border-box.shadow-4.code.pre.pre-wrap',
+        '',
+        {
+            class: [ 'self-stretch', 'f7', 'f6-ns', 'pa1', 'pa2-ns', 'mv1',
+                'ba', 'b--black-10', 'border-box', 'shadow-4', 'code', 'pre',
+                'pre-wrap' ].join(' '),
+        },
         result.status === Kanjiapi.SUCCESS ?
             JSON.stringify(result.value, null, 2) :
             result.status === Kanjiapi.LOADING ? 'Loading' :
@@ -132,14 +162,14 @@ const KANJI_FIELDS = [
         name: 'grade',
         description: [
             'The official grade of the kanji (1-6 for ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/Ky%C5%8Diku_kanji' }, 'Kyōiku kanji'),
+            m(Link, { href: KYOIKU_URL }, 'Kyōiku kanji'),
             ', 8 for the remaining ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/J%C5%8Dy%C5%8D_kanji' }, 'Jōyō kanji'),
+            m(Link, { href: JOYO_URL }, 'Jōyō kanji'),
             ', 9/10 for ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/Jinmeiy%C5%8D_kanji' }, 'Jinmeiyō kanji'),
+            m(Link, { href: JINMEIIYO_URL }, 'Jinmeiyō kanji'),
             ')',
         ],
-        type: [m('.di.f7', '1..6 | 8..10 '), 'number'],
+        type: [ m('.di.f7', '1..6 | 8..10 '), 'number' ],
     },
     {
         name: 'stroke_count',
@@ -170,16 +200,16 @@ const KANJI_FIELDS = [
         name: 'jlpt',
         description: [
             'The ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/Japanese-Language_Proficiency_Test#Previous_format_(1984%E2%80%932009)' }, 'former JLPT'),
+            m(Link, { href: JLPT_URL }, 'former JLPT'),
             ' test level for the kanji',
         ],
-        type: [m('.di.f7', '1..4 '), 'number'],
+        type: [ m('.di.f7', '1..4 '), 'number' ],
     },
     {
         name: 'unicode',
         description: [
             'The ',
-            m(Link, { href: 'https://en.wikipedia.org/wiki/Unicode' }, 'Unicode'),
+            m(Link, { href: UNICODE_URL }, 'Unicode'),
             ' codepoint of the kanji',
         ],
         type: 'string',
@@ -244,27 +274,24 @@ const VARIANT_FIELDS = [
 ]
 
 const SchemaRow = {
-    view: ({ attrs: { field, isLast } }) => m('.cf.pv2.pv0-ns.bb.b--silver', [
-        m(
-            '.fl.w-50.w-third-ns.pa1.code',
-            `"${field.name}":`,
-        ),
-        m(
-            '.fl.w-50.w-third-ns.pa1.code.small-caps.tr.tl-ns',
-            field.type,
-        ),
-        m(
-            '.f7.f6-ns.fl.w-100.w-third-ns.pa1.i',
-            field.description,
-        ),
-    ]),
+    view: ({ attrs: { field, isLast } }) => m(
+        '.cf.pv2.pv0-ns.bb.b--silver',
+        m('.fl.w-50.w-third-ns.pa1.code', `"${field.name}":`),
+        m('.fl.w-50.w-third-ns.pa1.code.small-caps.tr.tl-ns', field.type),
+        m('.f7.f6-ns.fl.w-100.w-third-ns.pa1.i', field.description),
+    ),
 }
 
 const Schema = {
     view: ({ attrs: { fields } }) => m(
         '.pa1.pa3-ns.mv2.ba.b--black-10.shadow-4',
-        [ 'field', 'type', 'description' ].map(heading => m('.fl.w-100.w-third-ns.f4.f3-ns.dn.db-ns.ph1.bb.b--silver', heading)),
-        fields.map((field, i) => m(SchemaRow, { field, isLast: (i === fields.length - 1) })),
+        [ 'field', 'type', 'description' ].map(heading => m(
+            '.fl.w-100.w-third-ns.f4.f3-ns.dn.db-ns.ph1.bb.b--silver',
+            heading,
+        )),
+        fields.map((field, i) => m(
+            SchemaRow, { field, isLast: (i === fields.length - 1) }
+        )),
     ),
 }
 
@@ -279,7 +306,10 @@ const EndpointDescription = {
 
 const Docs = {
     view: () => [
-        m('.self-center.mv2.tc.underline', m(m.route.Link, { href: '/', class: 'vermillion' }, 'home')),
+        m(
+            '.self-center.mv2.tc.underline',
+            m(InternalLink, { href: '/' }, 'home'),
+        ),
         m(
             EndpointDescription,
             {
@@ -323,22 +353,21 @@ function Home() {
     return {
         view: ({ attrs: { kanjiapi } }) => [
             m('.self-center.mv2.f4.f3-ns.tc', 'A modern JSON API for kanji'),
-            m('.self-center.mv2.tc.underline', m(
-                m.route.Link,
-                { href: '/documentation', class: 'vermillion' },
-                'documentation'),
+            m(
+                '.self-center.mv2.tc.underline',
+                m(InternalLink, { href: '/documentation' }, 'documentation'),
             ),
             m(
                 '.self-center.mv2.tc.underline',
                 m(
                     Link,
-                    { href: 'https://github.com/onlyskin/kanjiapi-wrapper' },
+                    { href: KANJIAPI_WRAPPER_URL },
                     'official wrapper library',
                 ),
             ),
             m(
                 '.self-center.mv2.tc',
-                m(Link, { href: 'https://kai.kanjiapi.dev' }, 'kanjikai'),
+                m(Link, { href: KANJIKAI_URL }, 'kanjikai'),
                 ': a webapp powered by kanjiapi.dev',
             ),
             m(Separator),
