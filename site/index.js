e600f4140ccc34a971a2761f60d58bde19c918e2
Violet Frost <121668049+violetfrost@users.noreply.github.com>
2023-02-07 18:15:27 -0500

    Add Tenori to projects list

--- site/index.js
+++ site/index.js
@@ -22,6 +22,7 @@ const EDRDG_URL = 'http://www.edrdg.org/'
 const EDRDG_LICENCE_URL = 'http://www.edrdg.org/edrdg/licence.html'
 const KANJIAPI_V1_URL = 'https://kanjiapi.dev/v1/'
 const KANJI_FLASH_URL = 'https://www.kanjiflash.com'
+const TENORI_URL = 'https://violetfrost.net/tenori'
 
 const Link = {
     view: ({ children, attrs: { href } }) => m(
@@ -122,6 +123,11 @@ const About = {
             '.self-start.pv2',
             m(Link, { href: KANJI_FLASH_URL}, 'kanji flash'),
             ': kanji flashcards by JLPT level and more'
+        ),
+        m(
+            'self-start.pv2',
+            m(Link, { href: TENORI_URL }, 'tenori'),
+            ': a cross-platform kanji memorization tool based on electron'
         ),
         m(
             '.self-start.pv2',
